2805c3ef69c41ff61c615f483644dabb99308482
iDz <bdls1704@hotmail.com>
2023-01-01 23:20:52 -0300

    update

--- src/utils/aws-s3-helper.ts
+++ src/utils/aws-s3-helper.ts
@@ -1,14 +1,20 @@
 import { S3 } from 'aws-sdk';
 
 const {
-  AWS_REGION = 'us-east-1',
-  AWS_S3_BUCKET = 'mtasa-launcher',
+  USE_AWS_REGION = '',
+  USE_AWS_S3_BUCKET = '',
+  USE_AWS_ACCESS_KEY_ID = '',
+  USE_AWS_SECRET_ACCESS_KEY = '',
 } = process.env;
 
 export class AWS_S3_HELPER {
   constructor(
     private client: S3 = new S3({
-      region: AWS_REGION,
+      region: USE_AWS_REGION,
+      credentials: {
+        accessKeyId: USE_AWS_ACCESS_KEY_ID,
+        secretAccessKey: USE_AWS_SECRET_ACCESS_KEY,
+      },
     })
   ) { }
 
@@ -29,7 +35,7 @@ export class AWS_S3_HELPER {
 
     const result = await this.client.upload({
       ACL: "public-read",
-      Bucket: AWS_S3_BUCKET,
+      Bucket: USE_AWS_S3_BUCKET,
       Body: file.data,
       Key: fileName,
       ContentType: file.mimetype,
@@ -42,7 +48,7 @@ export class AWS_S3_HELPER {
     const { folder, fileName } = params;
 
     const result = await this.client.deleteObject({
-      Bucket: AWS_S3_BUCKET,
+      Bucket: USE_AWS_S3_BUCKET,
       Key: `${folder}/${decodeURIComponent(fileName)}`,
     }).promise();
 
